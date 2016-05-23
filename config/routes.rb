27581c7362a5a129f48fc3a5fe352c6d9b0834d0
Brianna Veenstra <b.g.veenstra@gmail.com>
2016-05-23 12:07:43 -0700

    nest talk routes inside conference routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :talks
-  resources :conferences
+  
+
+
+  resources :conferences do
+    resources :talks
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
