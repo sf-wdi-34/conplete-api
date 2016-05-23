a76c4d20b20bc1e860e80a445ee4475c9ef9a063
Brianna Veenstra <b.g.veenstra@gmail.com>
2016-05-23 11:51:25 -0700

    fix typo in migration

--- db/migrate/20160523181933_create_talks.rb
+++ db/migrate/20160523181933_create_talks.rb
@@ -3,7 +3,7 @@ class CreateTalks < ActiveRecord::Migration
     create_table :talks do |t|
       t.string :title
       t.string :speaker_name
-      t.datetime, :start_time
+      t.datetime :start_time
       t.datetime :end_time
       t.belongs_to :conference, index: true, foreign_key: true
 
